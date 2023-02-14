384b1fe71b03c5db62ffba23fc089f9c3bf5dc23
Mia Tanner <mia@indygecko.com>
2023-02-14 14:33:10 -0800

    add authenticate_with_credentials method to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,15 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true
+
+
+  def authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    if user.authenticate(password)  
+      return user
+    else
+      return nil
+    end
+  end
+
 end
