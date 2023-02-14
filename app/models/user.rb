735443e0cff22bf301c90258a1a5c02841858b97
Mia Tanner <mia@indygecko.com>
2023-02-14 14:16:01 -0800

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :password, confirmation: true, length: {minimum: 6}
+  validates :password_confirmation, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :name, presence: true
 end
